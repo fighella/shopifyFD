05852b5eae1d531d06a7736c6a674d7e2610bf60
Jonny Dalgleish <fighella@gmail.com>
2016-01-29 21:07:03 +1100

    2

--- shopifyFD.js
+++ shopifyFD.js
@@ -28,7 +28,6 @@ if (typeof jQuery !== 'undefined') {
 if(!$('#shopifyjs').length){
 var d = document,url = d.URL;
 if(url.indexOf("myshopify.com/admin")>1){
-	alert('123');
 
 	var _savelabel = 'Save',
 	_editlabel = 'Edit',
